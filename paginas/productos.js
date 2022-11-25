8bb3a149f273d87014bfebbe1bb1b071a4d151b7
ricardosn <ricardoosn@gmail.com>
2022-11-25 00:05:35 -0300

    Nueva actualizacion

--- paginas/productos.js
+++ paginas/productos.js
@@ -1,37 +1,39 @@
 
 
 class Productos {
-    constructor(producto, precio, stock,){
+    constructor(producto, precio, stock, imagen){
         this.producto = producto;
         this.precio = precio;
         this.stock = stock;
+        this.imagen = imagen;
    
     } 
 }
 
 
    
-const panIntegral = new Productos("Pan integral", 360, 3, );
-const panCenteno = new Productos("Pan de centeno", 200,4);
-const panLino = new Productos("Pan de lino", 200,4);
-const panMaiz = new Productos("Pan de maiz", 150,3);
+const panIntegral = new Productos("Pan Integral", 360, 3,"../imagenes/paneIntegral.jpg");
+const brotchen = new Productos("Brotchen", 200,4,"../imagenes/Brotchen.jpg");
+const panNueces = new Productos("Pan de Nueces", 200,4,"../imagenes/PanNueces.jpg");
+const panMaiz = new Productos("Pan de Maiz", 150,3,"../imagenes/panMaiz.jpg");
 
-const arrayProductos = [panIntegral,panCenteno, panLino, panMaiz];
+const arrayProductos = [panIntegral,brotchen, panNueces, panMaiz];
 
 const contenedorProductos = document.getElementById("contenedorProductos");
 
 arrayProductos.forEach(Productos => {
     let div = document.createElement("div");
     div.innerHTML = `   <div class="card" style="width: 18rem;">
-    <img src="../imagenes/paneIntegral.jpg" alt="">"
+    <img src="${Productos.imagen}" alt="">"
     <div class="card-body">
       <h5 class="card-title">${Productos.producto}</h5>
       <p> precio: ${Productos.precio}.</p>
       <p> precio: ${Productos.stock}.</p>
-      <bottom id="btn"> comprar </bottom>
+      <button type="button" class="btn btn-primary" id="btn">Comprar</button>
     </div>
   </div> ` 
     contenedorProductos.appendChild(div);
+   
 })
 
 const btn = document.getElementById("btn");
@@ -39,6 +41,7 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", (e) => {
   e.preventDefault();
     alert("haz añadido el producto al carrito") 
+    compra.push(array.productos);
     
   });
  
